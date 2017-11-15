433a55b97aa5f6746cba214734983581c1632c91
Lony <valonshatrolli@live.com>
2017-11-15 23:41:58 +0100

    ListActivity greeting message

--- app/src/main/java/com/example/lony/edx1/MainActivity.java
+++ app/src/main/java/com/example/lony/edx1/MainActivity.java
@@ -6,65 +6,72 @@
 package com.example.lony.edx1;
 
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends ListActivity {
 
-    Button greetButton;
-    int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+    String[] names;
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent in = new Intent(this,ShowMessage.class);
+        in.putExtra("message",greetingByTime(names[(int) id]));
+        startActivity(in);
+    }
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        greetButton = (Button) findViewById(R.id.greetButton);
-
-        greetButton.setOnClickListener((a)->{
-            Intent in = new Intent(this,ShowMessage.class);
-
-            EditText editFriendName = (EditText) findViewById(R.id.editFriendName);
-            String friendName = editFriendName.getText().toString();
+        names = getResources().getStringArray(R.array.friends);
+        setListAdapter((ListAdapter)new ArrayAdapter<String>(this,R.layout.friend_item,names));
 
 
 
 
-            System.out.println(getString(R.string.goodafternoon));
+    }
 
-            String greetingMsg;
+    protected String greetingByTime(String name) {
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
 
-            if (hour>6&&hour<=12)
-                greetingMsg=getString(R.string.goodmorning);
-            else if (hour <= 17)
-                greetingMsg=getString(R.string.goodafternoon);
-            else if (hour <=21)
-                greetingMsg=getString(R.string.goodevening);
-            else
-                greetingMsg=getString(R.string.goodnight);
+        String greetingMsg;
 
+        if (hour>6&&hour<=12)
+            greetingMsg=getString(R.string.goodmorning);
+        else if (hour <= 17)
+            greetingMsg=getString(R.string.goodafternoon);
+        else if (hour <=21)
+            greetingMsg=getString(R.string.goodevening);
+        else
+            greetingMsg=getString(R.string.goodnight);
 
-            in.putExtra("message", greetingMsg+" "+friendName+"!");
-            startActivity(in);
+        return greetingMsg + " " + name + "!";
 
-
-
-
-
-
-
-        } );
     }
 
 
+
 }
+
+
+
+
